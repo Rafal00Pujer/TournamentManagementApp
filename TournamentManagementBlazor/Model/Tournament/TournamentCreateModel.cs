c4f36d3892a39135db59511bd61ac93567266b0c
RafalPujer <rafal.pujer00@gmail.com>
2023-09-18 19:27:12 +0200

    Added create tournament feature.

---
+++ TournamentManagementBlazor/Model/Tournament/TournamentCreateModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TournamentManagementBlazor.Model.Tournament;
+
+public class TournamentCreateModel
+{
+    [Display(Name = "Tournament Name")]
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    public string TournamentName { get; set; } = string.Empty;
+
+    [Display(Name = "Game Name")]
+    [StringLength(50)]
+    public string? GameName { get; set; }
+
+    [Display(Name = "Tournament Description")]
+    [StringLength(500)]
+    public string? TournamentDescription { get; set; }
+
+    [Display(Name = "Teams")]
+    [Required]
+    [ValidateComplexType]
+    [MinLength(1, ErrorMessage = "Add minimum one team.")]
+    public IList<TournamentCreateTeamModel> Teams { get; set; } = new List<TournamentCreateTeamModel> { new() };
+}
---
+++ TournamentManagementBlazor/Model/Tournament/TournamentCreateTeamModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TournamentManagementBlazor.Model.Tournament;
+
+public class TournamentCreateTeamModel
+{
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
+    [Display(Name = "Team Name")]
+    public string Name { get; set; } = string.Empty;
+}
